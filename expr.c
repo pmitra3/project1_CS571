941d858e3070a7423885dcc87fec71c7435dd0b8
Pallabi Mitra pmitra3 <pmitra3@binghamton.edu>
2025-09-18 22:02:35 -0400

    New changes

--- expr.c
+++ expr.c
@@ -1,12 +1,15 @@
+/* Collaborators: None*/
 #include "expr.h"
 #include "stdlib.h"
 #include "stdio.h"
+
 
 struct Expr *mk_plus(struct Expr *e1, struct Expr *e2) {
     struct Expr *ret = malloc(sizeof(struct Expr));
     ret->type = PLUS;
     ret->subexprs.e1 = e1;
     ret->subexprs.e2 = e2;
+
     return ret;
 }
 
@@ -15,6 +18,7 @@ struct Expr *mk_minus(struct Expr *e1, struct Expr *e2) {
     ret->type = MINUS;
     ret->subexprs.e1 = e1;
     ret->subexprs.e2 = e2;
+
     return ret;
 }
 
@@ -23,6 +27,7 @@ struct Expr *mk_times(struct Expr *e1, struct Expr *e2) {
     ret->type = TIMES;
     ret->subexprs.e1 = e1;
     ret->subexprs.e2 = e2;
+
     return ret;
 }
 
@@ -31,6 +36,7 @@ struct Expr *mk_div(struct Expr *e1, struct Expr *e2) {
     ret->type = DIV;
     ret->subexprs.e1 = e1;
     ret->subexprs.e2 = e2;
+
     return ret;
 }
 
@@ -38,6 +44,7 @@ struct Expr *mk_float(float f) {
     struct Expr *ret = malloc(sizeof(struct Expr));
     ret->type = FLOAT;
     ret->literal = f;
+
     return ret;
 }
 
@@ -45,57 +52,55 @@ struct Expr *mk_float(float f) {
  * using the above constructors.
  */
 struct Expr *mk_expr1() {
-    // Create (5 * 3) first
-    struct Expr *five_times_three = mk_times(mk_float(5.0), mk_float(3.0));
-    
-    // Then create (2 + (5 * 3))
-    return mk_plus(mk_float(2.0), five_times_three);
+     struct Expr *five = mk_float(5.0f);
+    struct Expr *three = mk_float(3.0f);
+    struct Expr *two = mk_float(2.0f);
+
+    struct Expr *five_times_three = mk_times(five, three);
+    struct Expr *expr = mk_plus(two, five_times_three);
+    return expr;
 }
 
 /* This function should create the expr (3 + (1 / 8))
  * using the above constructors.
  */
 struct Expr *mk_expr2() {
-    // Create (1 / 8) first
-    struct Expr *one_div_eight = mk_div(mk_float(1.0), mk_float(8.0));
-    
-    // Then create (3 + (1 / 8))
-    return mk_plus(mk_float(3.0), one_div_eight);
+    struct Expr *three = mk_float(3.0f);
+    struct Expr *one = mk_float(1.0f);
+    struct Expr *eight = mk_float(8.0f);
+
+    struct Expr *one_div_eight = mk_div(one, eight);
+    struct Expr *expr = mk_plus(three, one_div_eight);
+    return expr;
 }
 
 /* This function should create the expr ((4 / 3) - (4 / (2 + 3)))
  * using the above constructors.
  */
 struct Expr *mk_expr3() {
-    // Create (4 / 3)
-    struct Expr *four_div_three = mk_div(mk_float(4.0), mk_float(3.0));
-    
-    // Create (2 + 3)
-    struct Expr *two_plus_three = mk_plus(mk_float(2.0), mk_float(3.0));
-    
-    // Create (4 / (2 + 3))
-    struct Expr *four_div_sum = mk_div(mk_float(4.0), two_plus_three);
-    
-    // Finally create ((4 / 3) - (4 / (2 + 3)))
-    return mk_minus(four_div_three, four_div_sum);
+   struct Expr *four = mk_float(4.0f);
+    struct Expr *three = mk_float(3.0f);
+    struct Expr *two = mk_float(2.0f);
+
+    struct Expr *four_div_three = mk_div(four, three);
+    struct Expr *two_plus_three = mk_plus(two, three);
+    struct Expr *four_div_sum = mk_div(four, two_plus_three);
+    struct Expr *expr = mk_minus(four_div_three, four_div_sum);
+    return expr;
 }
+
 
 /*
  * This function should free all memory associated 
  * with the given AST.
 */
 void free_expr(struct Expr* e) {
-    if (e == NULL) {
-        return;
-    }
-    
-    // If this is not a leaf node (FLOAT), recursively free children
-    if (e->type != FLOAT) {
+     if (!e) return;
+
+    if (e->type == PLUS || e->type == MINUS || e->type == TIMES || e->type == DIV) {
         free_expr(e->subexprs.e1);
         free_expr(e->subexprs.e2);
     }
-    
-    // Free the current node
     free(e);
 }
 
@@ -104,28 +109,21 @@ void free_expr(struct Expr* e) {
  * return the floating-point result.
 */
 float eval(struct Expr* e) {
-    if (e == NULL) {
-        return 0.0;  // Should not happen in well-formed expressions
-    }
-    
+    if (!e) return 0.0f;
+
     switch (e->type) {
-        case FLOAT:
-            return e->literal;
-            
         case PLUS:
             return eval(e->subexprs.e1) + eval(e->subexprs.e2);
-            
         case MINUS:
             return eval(e->subexprs.e1) - eval(e->subexprs.e2);
-            
         case TIMES:
             return eval(e->subexprs.e1) * eval(e->subexprs.e2);
-            
         case DIV:
             return eval(e->subexprs.e1) / eval(e->subexprs.e2);
-            
+        case FLOAT:
+            return e->literal;
         default:
-            // Should not happen
-            return 0.0;
-    }
+            return 0.0f; // Should not happen
+}
+
 }
